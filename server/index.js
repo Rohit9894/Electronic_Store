d195f2cd349795ba05a5223e79285e45b0b6a764
Rohit9894 <Princerohit9893@gmail.com>
2025-05-19 16:42:04 +0530

    client url updated

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 4000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 /**
  * Swagger Implementation
